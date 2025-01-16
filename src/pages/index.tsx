f59179a4ff78eb7060f8b2d9217afdc8e9a65a2d
Jorge Raul Ochoa-Reyes <jorgeochoareyes@gmail.com>
2025-01-16 15:16:06 -0800

    added fix to app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
-import { signIn, signOut, useSession } from "next-auth/react";
-import { ModeToggle } from "~/components/theme-changer";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "~/components/ui/breadcrumb";
 import { Separator } from "~/components/ui/separator";
-import { SidebarTrigger } from "~/components/ui/sidebar";
-
-import { api } from "~/utils/api";
+import { SidebarTrigger } from "~/components/ui/sidebar"; 
 
 export default function Home() { 
 
@@ -40,27 +36,4 @@ export default function Home() {
     </>
   );
 }
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-}
+ 
